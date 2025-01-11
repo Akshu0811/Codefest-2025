baae0758ec05f0a7eeb29fee4089249698331fb3
Akshu0811 <akshayasivaguru08@gmail.com>
2025-01-11 10:29:47 +0530

    Added code

---
+++ Gmail Client Server.py
@@ -0,0 +1,12 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def send_email(sender_email, sender_password,recipient_email, subject, body ):
+     try:
+     message = MIMEMultipart
+     message = ["from"]= sender_email
+     message = ["To"]= recipient_email
+     message = ["Subject"]= subject
+
+    
