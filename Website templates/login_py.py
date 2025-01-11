07f7221d9bbfae07b79f7522dc4434b99a22a4b6
Akshu0811 <akshayasivaguru08@gmail.com>
2025-01-12 02:51:02 +0530

    updated

--- Website templates/login_py.py
+++ Website templates/login_py.py
@@ -2,22 +2,22 @@
 import json
 import os
 
-def load_emails(login_py="emails.json"):
-    if os.path.exists(login_py):  # Check if the file exists
-        with open(login_py, "r") as file:
+def load_emails(submit_email="emails.json"):
+    if os.path.exists(submit_email):  # Check if the file exists
+        with open(submit_email, "r") as file:
             return json.load(file)  # Load the email data from the file
     else:
         return []  # Return an empty list if no file exists
 
-def save_emails(emails, login_py="emails.json"):
-    with open(login_py, "w") as file:
+def save_emails(emails, filename="emails.json"):
+    with open(filename, "w") as file:
         json.dump(emails, file)  # Save the emails list into the file
 
-def add_email(new_email, login_py="emails.json"):
-    emails = load_emails(login_py)  # Load current emails
+def add_email(new_email, filename="emails.json"):
+    emails = load_emails(filename)  # Load current emails
     if new_email not in emails:  # Check if the email already exists
         emails.append(new_email)  # Add the new email
-        save_emails(emails, login_py)  # Save the updated list
+        save_emails(emails, filename)  # Save the updated list
         print(f"Email {new_email} added successfully!")
     else:
         print(f"Email {new_email} already exists!")  # Inform the user if email exists
