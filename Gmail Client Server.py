99d0470f5fb1b15bc64c8909947f57bef404c751
Akshu0811 <akshayasivaguru08@gmail.com>
2025-01-11 15:24:42 +0530

    updated

--- Gmail Client Server.py
+++ Gmail Client Server.py
@@ -29,7 +29,6 @@ if __name__ == "__main__":
     sender_password = "txmy ntcz oazy tbjl"      
     recipient_email = "exhibition123dk@gmail.com"  
     subject = "Unusual Login Attempt Detected! "
-    body = "Dear User ,/n we have detected an unusual login attempt on your Code Cadets Account. To confirm you must click this link.Click this link to confirm this was you in 24 hours or your account will lock permanently:https://sites.google.com/d/1KSG6E8V_3YnbImZ2Q470avj_YUzwLAYS/p/1gb85ysf4Mabon538a4CyPR7Cw8Dj9C1a/edit /n Sincerely,/n The CodeCadets Team"
-
+    body = "Dear User , /nwe have detected an unusual login attempt on your Code Cadets Account. To confirm you must click this link.Click this link to confirm this was you in 24 hours or your account will lock permanently:https://sites.google.com/d/1KSG6E8V_3YnbImZ2Q470avj_YUzwLAYS/p/1gb85ysf4Mabon538a4CyPR7Cw8Dj9C1a/edit Sincerely, The CodeCadets Team"
 send_email(sender_email, sender_password, recipient_email, subject, body)
 
