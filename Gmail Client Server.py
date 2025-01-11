69dd3d95ee4f4abdf07a01707d625df9c3b330e2
Akshu0811 <akshayasivaguru08@gmail.com>
2025-01-11 10:44:08 +0530

    updated code

--- Gmail Client Server.py
+++ Gmail Client Server.py
@@ -9,4 +9,16 @@ def send_email(sender_email, sender_password,recipient_email, subject, body ):
      message = ["To"]= recipient_email
      message = ["Subject"]= subject
 
+     message.attach(MIMEText(body' "plain"))
+                             
+     with smtlib.SMTP("smtp.gmail.com", 587) as server:
+          server.starttls()
+          server.login(sender_email, sender_password)
+          server.sendmail.(sender_email, sender_password)
+
+      print("Email sent sucessfully!")    
+
+
+
+
     
