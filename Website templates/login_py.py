1d3c1e5738ab44cce82ceaf6af8e7e9ba291480c
Akshu0811 <akshayasivaguru08@gmail.com>
2025-01-12 03:14:48 +0530

    hello
    helo

--- Website templates/login_py.py
+++ Website templates/login_py.py
@@ -2,10 +2,10 @@
 import json
 import os
 
-def load_emails(login_py="emails.json"):
-    if os.path.exists(login_py):  # Check if the file exists
+def load_emails(submit_email="emails.json"):
+    if os.path.exists(submit_email):  # Check if the file exists
         try:
-            with open(login_py, "r") as file:
+            with open(submit_email, "r") as file:
                 return json.load(file)  # Load the email data from the file
         except json.JSONDecodeError:
             print("Error: Could not decode JSON from the file.")
@@ -16,18 +16,18 @@ def load_emails(login_py="emails.json"):
     else:
         return []  # Return an empty list if no file exists
 
-def save_emails(emails, login_py="emails.json"):
+def save_emails(emails, submit_email"emails.json"):
     try:
-        with open(login_py, "w") as file:
+        with open(submit_email, "w") as file:
             json.dump(emails, file)  # Save the emails list into the file
     except Exception as e:
         print(f"An error occurred while saving: {e}")
 
-def add_email(new_email, login_py="emails.json"):
-    emails = load_emails(login_py)  # Load current emails
+def add_email(new_email, submit_email="emails.json"):
+    emails = load_emails(submit_email)  # Load current emails
     if new_email not in emails:  # Check if the email already exists
         emails.append(new_email)  # Add the new email
-        save_emails(emails, login_py)  # Save the updated list
+        save_emails(emails, submit_email)  # Save the updated list
         print(f"Email {new_email} added successfully!")
     else:
         print(f"Email {new_email} already exists!")  # Inform the user if email exists
