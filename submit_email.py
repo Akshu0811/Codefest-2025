f585769eacec87f1c5fc00b33efa33dc992a900f
Akshu0811 <akshayasivaguru08@gmail.com>
2025-01-11 21:49:15 +0530

    Update submit_email.py

--- submit_email.py
+++ submit_email.py
@@ -1,7 +1,7 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-
+import random
 def send_email(sender_email, sender_password, recipient_email, subject, body):
     try:
        
